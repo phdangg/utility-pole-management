bcb9395bff54d1687961b9bffcde6026da742229
phdangg <dangmanhphuc2003@gmal.com>
2024-10-14 15:58:46 +0700

    exception controller

---
+++ src/main/java/com/evn/utilitypolemanagement/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.evn.utilitypolemanagement.exceptions;
+
+import com.evn.utilitypolemanagement.exceptions.Pole.DuplicatePoleException;
+import com.evn.utilitypolemanagement.exceptions.Pole.PoleNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(DuplicatePoleException.class)
+    public ProblemDetail handleDuplicatePoleException(DuplicatePoleException e){
+        return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST,e.getMessage());
+    }
+    @ExceptionHandler(PoleNotFoundException.class)
+    public ProblemDetail handlePoleNotFoundException(PoleNotFoundException e){
+        return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND,e.getMessage());
+    }
+}
