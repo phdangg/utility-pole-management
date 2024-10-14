d7bb5e8fc4d783a70f8a45e956111218639c98be
phdangg <dangmanhphuc2003@gmal.com>
2024-10-14 14:25:37 +0700

    jwt config init

---
+++ src/main/java/com/evn/utilitypolemanagement/config/JwtTokenFilter.java
@@ -0,0 +1,77 @@
+package com.evn.utilitypolemanagement.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.crypto.SecretKey;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class JwtTokenFilter extends OncePerRequestFilter {
+
+    @Value("${security.jwt.secret-key}")
+    private String scecretKey;
+
+    private final SecretKey key = Keys.hmacShaKeyFor(scecretKey.getBytes());
+
+    @Override
+    protected void doFilterInternal(jakarta.servlet.http.HttpServletRequest request, jakarta.servlet.http.HttpServletResponse response, jakarta.servlet.FilterChain filterChain) throws jakarta.servlet.ServletException, IOException {
+        String header = request.getHeader("Authorization");
+
+        if (header == null || !header.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = header.substring(7);
+
+        try {
+            Claims claims = Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+
+            // Extract information from claims and set it in the SecurityContext
+            String username = claims.getSubject();
+            String aud = claims.getAudience();
+            if(aud.equals("SPC.HTVTCNTT")){
+                List<String> roles = claims.get("roles", List.class);
+
+                // Convert roles to SimpleGrantedAuthority list
+                List<SimpleGrantedAuthority> authorities = roles.stream()
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList());
+
+                if (username != null) {
+                    // Create an Authentication object
+                    var authentication = new UsernamePasswordAuthenticationToken(
+                            username,
+                            username,
+                            authorities // You can set authorities here
+                    );
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
+            }else{
+                SecurityContextHolder.clearContext();
+            }
+
+        } catch (Exception e) {
+            // If token is invalid or expired
+            SecurityContextHolder.clearContext();
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
