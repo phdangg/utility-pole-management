300057945e8350b4bae21cbba34d4683068430cf
phdangg <dangmanhphuc2003@gmal.com>
2024-10-14 14:16:48 +0700

    init

---
+++ src/main/java/com/evn/utilitypolemanagement/UtilitypolemanagementApplication.java
@@ -0,0 +1,13 @@
+package com.evn.utilitypolemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilitypolemanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtilitypolemanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evn/utilitypolemanagement/UtilitypolemanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evn.utilitypolemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilitypolemanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
