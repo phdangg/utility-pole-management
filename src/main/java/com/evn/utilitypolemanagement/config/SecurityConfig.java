74a783f966fb78fa9973a9eb2c5a01e7b114c4dc
Đặng Mạnh Phúc <dangmanhphuc2003@gmail.com>
2025-01-05 10:51:58 +0700

    pole persit

--- src/main/java/com/evn/utilitypolemanagement/config/SecurityConfig.java
+++ src/main/java/com/evn/utilitypolemanagement/config/SecurityConfig.java
@@ -4,9 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import java.util.List;
 
 @Configuration
 @EnableMethodSecurity
@@ -21,12 +24,20 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configurationSource(request -> {
+                    CorsConfiguration configuration = new CorsConfiguration();
+                    configuration.setAllowedOrigins(List.of("*"));
+                    configuration.setAllowedMethods(List.of("*"));
+                    configuration.setAllowedHeaders(List.of("*"));
+                    return configuration;
+                }))
+                .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                                .anyRequest().authenticated()
-                )
-                .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+                                //.anyRequest().authenticated()
+                                .anyRequest().permitAll()
+                );
+                //.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
--- src/test/java/com/evn/utilitypolemanagement/repositories/PoleRepositoryTest.java
+++ src/test/java/com/evn/utilitypolemanagement/repositories/PoleRepositoryTest.java
@@ -1,14 +1,34 @@
 package com.evn.utilitypolemanagement.repositories;
 
+import com.evn.utilitypolemanagement.entities.Pole;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
+import java.sql.Timestamp;
+
 @DataJpaTest
 public class PoleRepositoryTest {
     @Autowired
-    private TestEntityManager testEntityManager;
+    PoleRepository poleRepository;
+
+    @Test
+    void testSaveMethod_WhenSavePole_ReturnPole() {
+        Pole poleEntity = Pole.builder()
+                .poleName("poleName")
+                .poleShortName("poleShortName")
+                .isEnable(true)
+                .build();
+
+        Pole pole = poleRepository.save(poleEntity);
+
+        Assertions.assertEquals(poleEntity.getPoleName(), pole.getPoleName());
+        Assertions.assertEquals(poleEntity.getPoleShortName(), pole.getPoleShortName());
+        Assertions.assertEquals(poleEntity.getIsEnable(), pole.getIsEnable());
+
+    }
 
     @Test
     public void whenFindByPoleNameOrPoleShortName_thenReturnPole() {
